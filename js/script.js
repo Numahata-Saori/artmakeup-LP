abc4363671a8a4f89a9a84df7f3fadfbd228ab3a
numahata-saori <numahata.saori@gmail.com>
2023-04-29 01:19:38 +0900

    gallery add

--- js/script.js
+++ js/script.js
@@ -1,14 +1,76 @@
-// $(".header__menu__openbtn, header__menu__nav, header__menu__close").click(function () {
-//   $(".header__menu, header__menu__close").toggleClass('active');
+$(document).ready(function () {
+  $(".header__menu__openbtn").click(function () {
+    $(this).toggleClass("active2");
+    $("#g-nav").toggleClass("panelactive");
+  });
+
+  $("#g-nav a").click(function () {
+    $(".header__menu__openbtn").removeClass("active2");
+    $("#g-nav").removeClass("panelactive");
+  });
+
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+  if (isMobile) {
+    const swiper = new Swiper(".swiper-container", {
+      slidesPerView: "auto",
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+      },
+      touchStartPreventDefault: false,
+    });
+  }
+});
+
+
+// $(".header__menu__openbtn").click(function () {
+//   $(this).toggleClass('active2');
+//   $("#g-nav").toggleClass('panelactive')
 // });
 
+// $("#g-nav a").click(function () {
+//   $(".header__menu__openbtn").removeClass('active2');
+//   $("#g-nav").removeClass('panelactive');
+// });
 
-$(".header__menu__openbtn").click(function () {
-  $(this).toggleClass('active');
-  $("#g-nav").toggleClass('panelactive')
-});
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
 
-$("#g-nav a").click(function () {
-  $(".header__menu__openbtn").removeClass('active');
-  $("#g-nav").removeClass('panelactive');
-});
+//   // If we need pagination
+//   pagination: {
+//   el: '.swiper-pagination',
+//   type: 'bullets',
+//   clickable: true,
+//   },
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var isMobile = window.matchMedia('(max-width: 768px)').matches;
+//   if (isMobile) {
+//     var swiper = new Swiper('.swiper-container', {
+//       // slidesPerView: 'auto',
+//       slidesPerGroup: 1,
+//       spaceBetween: 10,
+//       loop: true,
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//       autoplay: {
+//         delay: 1000,
+//         disableOnInteraction: false,
+//       },
+//       touchStartPreventDefault: false,
+//     });
+//   }
+// });
